376a02448300aa5dc15200626c2209462a329f65
shreyasbangera <shreyasybangera1@gmail.com>
2023-05-02 13:01:19 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { images } from "./images";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
+import PauseCircleFilledIcon from '@mui/icons-material/PauseCircleFilled';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface images {
+  images: string;
+  id: number
 }
 
-export default App;
+const ImageCarousel = () => {
+  const [currentImage, setCurrentImage] = useState(images[0]);
+  const [play, setPlay] = useState(true)
+
+  useEffect(() => {
+    if(play){
+        var interval = setInterval(() => {
+        const currentIndex = images.findIndex((image) => image.id === currentImage.id);
+        const nextIndex = (currentIndex + 1) % images.length;
+        setCurrentImage(images[nextIndex]);
+    }, 3000);
+  }
+
+    return () => clearInterval(interval);
+  }, [currentImage, images.length, play]);
+
+  const goToNextSlide = () => {
+    const currentIndex = images.findIndex((image) => image.id === currentImage.id);
+    const nextIndex = (currentIndex + 1) % images.length;
+    setCurrentImage(images[nextIndex]);
+  };
+
+  const goToPreviousSlide = () => {
+    const currentIndex = images.findIndex((image) => image.id === currentImage.id);
+    const previousIndex = (currentIndex - 1 + images.length) % images.length;
+    setCurrentImage(images[previousIndex]);
+  };
+
+  return (
+    <div className="App">
+           <div className="carousel-info">
+            <div className="img-container">
+           <img src ={currentImage.img} className="carousel-image"/>
+           </div>
+           <p className="carousel-text">{currentImage.text}</p>
+         </div>
+      <div className="carousel">
+        <span onClick={goToPreviousSlide} style={{ cursor: "pointer" }}>
+          <ArrowBackIosIcon />
+        </span>
+        {images.map(({img,id})=>(
+          <img className={`img ${id===currentImage.id?"active":"inactive"}`} src={img} alt={`Slide ${currentImage}`} />
+        ))}
+        <span onClick={goToNextSlide} style={{ cursor: "pointer" }}>
+          <ArrowForwardIosIcon />
+        </span>
+      <span style={{marginLeft:"80px", cursor:"pointer"}} onClick={()=>setPlay(!play)}>{play?<PauseCircleFilledIcon fontSize="large"/>:<PlayCircleFilledWhiteIcon fontSize="large"/>}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ImageCarousel;
---
+++ src/images.tsx
@@ -0,0 +1,27 @@
+export const images = [
+  { 
+    id:1,
+    img: "https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:2, 
+    img: "https://images.pexels.com/photos/593655/pexels-photo-593655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    text:"Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id:3,
+    img: "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    text:"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    id:4,
+    img: "https://images.pexels.com/photos/381739/pexels-photo-381739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    text:"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+  },
+  {
+    id:5,
+    img: "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    text:"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+];
